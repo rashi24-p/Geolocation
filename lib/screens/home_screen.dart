1b24aff96a36fb9f999a0e7c14a4e9c66f875901
Rashi Porwal <porwalrashi786@gmail.com>
2023-12-24 16:57:31 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:geolocation/screens/current_location_screen.dart';
+import 'package:geolocation/screens/simple_map_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Find Your Job through map."),
+          centerTitle: true,
+        ),
+        body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return const SimpleMapScreen();
+                      }));
+                    },
+                    child: const Text("Job Corner")),
+
+                    ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return const CurrentLocationScreen();
+                      }));
+                    },
+                    child: const Text("User Current Location")),
+                     ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return const CurrentLocationScreen();
+                      }));
+                    },
+                    child: const Text("Search Jobs near you")),
+              ],
+            )));
+  }
+}
